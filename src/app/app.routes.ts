661ebda6f574ac3c40163d915532e4659ad459f5
sadik <sadiks2712@>
2026-01-31 12:56:09 +0530

    Fix report route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { ComplaintComponent } from './complaint/complaint';
 
 export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'complaint',
+    component: ComplaintComponent
   },
   {
     path: '',
